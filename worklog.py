8a34e4dc9641e567d56030d9427ed1b8fda976d2
Ron Tovbin <ron.tovbin@gmail.com>
2018-04-27 16:46:30 -0400

    fixed the program to initialize the model

--- worklog.py
+++ worklog.py
@@ -1,5 +1,8 @@
 from mainmenu import *
+from models import initialize
 
 if __name__ == "__main__":
+    # initialize the database, if setup already it will use existing
+    initialize()
     main_menu = MainMenu()
     main_menu.menu_loop()
